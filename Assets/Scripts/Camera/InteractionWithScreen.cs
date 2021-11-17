d6a48eca3aa156371f6eea0f32e883dc6ccf2f39
Vasiliy <v.nichnikov@gmail.com>
2021-11-17 21:21:27 +0300

    A script has been written to get the cursor position when clicking on the LMB

---
+++ Assets/Scripts/Camera/InteractionWithScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class InteractionWithScreen : MonoBehaviour
+{
+    private Camera _camera;
+    private Vector3 _positionLeftBtn;
+    public Vector3 PositionLeftBtn => _positionLeftBtn;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _positionLeftBtn = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(new Vector3(_positionLeftBtn.x, _positionLeftBtn.y, 0), 0.2f);
+    }
+}
