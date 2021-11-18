f071313a5f6fcf1a905264190e3d8e2b4cdb30f3
Vasiliy <v.nichnikov@gmail.com>
2021-11-18 15:29:33 +0300

    Blocking cursor exit from the screen

--- Assets/Scripts/Camera/InteractionWithScreen.cs
+++ Assets/Scripts/Camera/InteractionWithScreen.cs
@@ -7,7 +7,7 @@ namespace Camera
     public class InteractionWithScreen : MonoBehaviour
     {
         [SerializeField] private PlayerEngine _playerEngine;
-        
+
         private UnityEngine.Camera _camera;
         private Vector3 _positionLeftBtn;
 
@@ -18,12 +18,30 @@ namespace Camera
 
         private void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                _positionLeftBtn = _camera.ScreenToWorldPoint(Input.mousePosition);
-                _positionLeftBtn.z = 0;
-                _playerEngine.Move(_positionLeftBtn);
-            }
+            if (Input.GetMouseButton(0) == false) return;
+            _positionLeftBtn = GetCorrectPositionMouse(Input.mousePosition);
+            _playerEngine.Move(_positionLeftBtn);
+        }
+
+        private Vector3 GetCorrectPositionMouse(Vector3 position)
+        {
+            float x = position.x;
+            float y = position.y;
+            float z = 0.0f;
+
+            if (x < 0)
+                x = 0;
+            else if (x > Screen.width)
+                x = Screen.width;
+
+            if (y < 0)
+                y = 0;
+            else if (y > Screen.height)
+                y = Screen.height;
+
+            Vector3 worldPositionMouse = _camera.ScreenToWorldPoint(new Vector3(x, y, z));
+            worldPositionMouse.z = 0;
+            return worldPositionMouse;
         }
     }
 }
